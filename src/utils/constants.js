f5c9c899dcf23ed73c02d37f49c3577093ec829d
Jahongir Yusupov <jahongir.yusupov.94@icloud.com>
2024-08-12 11:24:20 +0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,5 @@ export const tg = {
 };
 
 export const BASE_URL_TG = `https://api.telegram.org/bot${tg.token}/sendMessage?chat_id=${tg.chat_id}&text=`;
-export const BASE_URL = 'https://waterwell-monitor-production.up.railway.app/api/';
+export const BASE_URL = 'https://waterwell-monitor-production-cf83.up.railway.app/api/';
 export const privatRoutes = ['/super-user-profile'];
