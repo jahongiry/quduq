0f2c2c7d27a3133bf60b6389a22f39491eea02f1
Murodulla <yokubjanovich@Murodullas-MacBook-Air.local>
2024-10-23 01:07:54 +0500

    fixed date

--- src/components/datapicker/DataPicker.jsx
+++ src/components/datapicker/DataPicker.jsx
@@ -1,31 +1,30 @@
-import { DatePickerInput } from '@mantine/dates'
-import PropTypes from 'prop-types'
+import { DatePickerInput } from "@mantine/dates";
+import PropTypes from "prop-types";
 
-export const DataPicker = ({ mem, setMem }) => {
-	return (
-		<DatePickerInput
-			clearable
-			defaultValue={new Date()}
-			value={mem}
-			onChange={setMem}
-			label='Pick date'
-			placeholder='Vaqtni tanlang'
-			color='blue'
-			styles={{
-				input: {
-					backgroundColor: '#eee',
-					color: '#333',
-				},
-				dropdown: {
-					backgroundColor: '#333',
-				},
-			}}
-		/>
-	)
-}
-
+export const DataPicker = ({ now, setNow }) => {
+  return (
+    <DatePickerInput
+      clearable
+      defaultValue={new Date()}
+      value={now}
+      onChange={setNow}
+      label="Pick date"
+      placeholder="Vaqtni tanlang"
+      color="blue"
+      styles={{
+        input: {
+          backgroundColor: "#eee",
+          color: "#333",
+        },
+        dropdown: {
+          backgroundColor: "#333",
+        },
+      }}
+    />
+  );
+};
 
 DataPicker.propTypes = {
-	mem: PropTypes.instanceOf(Date).isRequired,
-	setMem: PropTypes.func.isRequired,
+  now: PropTypes.instanceOf(Date).isRequired,
+  setNow: PropTypes.func.isRequired,
 };
